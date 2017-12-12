90d166c80382b6de33b2b34ad571bc428a8a0519
daheige <zhuwei313@hotmail.com>
2017-12-12 18:55:26 +0800

    给通道使用for遍历

--- gobase/currency/gochan3.go
+++ gobase/currency/gochan3.go
@@ -9,8 +9,11 @@ func main() {
 	// stream := sumpFactory()
 	// go reviceSump(stream)
 
-	stream_2 := setMoreCh()
-	go getMoreCh(stream_2)
+	// stream_2 := setMoreCh(2)
+	// getMoreCh(stream_2)
+
+	//两个协程之间通信
+	getMoreCh(setMoreCh(3))
 	time.Sleep(1e9) //停顿1s让通道的收发在1s之内完成
 }
 
@@ -32,8 +35,7 @@ func reviceSump(ch chan int) {
 }
 
 //用函数来生成一个通道并返回（工厂角色）
-func setMoreCh() chan int {
-	var buf = 12
+func setMoreCh(buf int) chan int {
 	ch := make(chan int, buf)
 	go func() {
 		for i := 0; i < buf; i++ {
@@ -52,7 +54,15 @@ func getMoreCh(ch chan int) {
 
 	//利用for...range来遍历通道中的值
 	//for 循环的 range 语句可以用在通道 ch 上，便可以从通道中获取值
-	for v := range ch {
-		fmt.Println(v)
-	}
+	/*for v := range ch {
+	      fmt.Println(v)
+	  }
+	*/
+
+	//for它从指定通道中读取数据直到通道关闭，才继续执行下边的代码
+	go func() {
+		for v := range ch {
+			fmt.Println(v)
+		}
+	}()
 }
