b68190865f083293923438751a31c1bbdb5da790
daheige <zhuwei313@hotmail.com>
2018-02-04 13:48:29 +0800

    db查询

--- goweb/part6/sql/demo.go
+++ goweb/part6/sql/demo.go
@@ -38,15 +38,16 @@ func main() {
 
 	getData()
 
+	t := time.Now().Unix()
+	rnd_str := strconv.Itoa(int(t)) //将时间戳转换为int类型,最后转换为字符串
 	//更新数据可以用db.Exec来处理,受到影响的函数获取,使用RowsAffected方法
-	ret3, _ := db.Exec("update test set name ='haha3' where id = ?", 12)
+	ret3, _ := db.Exec("update test set name ='haha3"+rnd_str+"' where id = ?", 12)
 	aff_rows, _ := ret3.RowsAffected()
 	fmt.Println("影响的行数是", aff_rows)
 	//删除数据也可用db.Exec来处理，返回处理受到影响的行数RowsAffected
+	//对于insert,update,delete都可以用db.Exec来处理
 
 	//事务处理
-	t := time.Now().Unix()
-	rnd_str := strconv.Itoa(int(t)) //将时间戳转换为int类型,最后转换为字符串
 	tx, _ := db.Begin()
 	r4, _ := tx.Exec("update test set name = 'heige3333"+rnd_str+"' where id = ?", 13)
 	r5, _ := tx.Exec("update test set name = 'heige3314"+rnd_str+"' where id = ?", 14)
@@ -60,6 +61,8 @@ func main() {
 		fmt.Println("执行失败！")
 	}
 
+	// db.SetMaxIdleConns(n int) //设置连接池中能够保持的最大空闲连接的数量
+	show_process()
 }
 
 func checkError(err error) {
@@ -102,8 +105,7 @@ func getData() {
 		r := make(map[string]string)
 		//把vals中的数据复制到r
 		for k, v := range vals {
-			key := cols[k]
-			r[key] = string(v)
+			r[cols[k]] = string(v)
 		}
 		result[i] = r
 		// fmt.Println(r)
@@ -112,9 +114,49 @@ func getData() {
 	fmt.Println("第一行数据是", result[0]["id"], result[0]["name"])
 
 	//查询一条记录
-	row := db.QueryRow("select id,name from test limit ?", 1)
+	row := db.QueryRow("select id,name from test where id > ? order by id desc limit ?", 18, 1)
 	user := UserInfo{} //user的结构是一个结构体的一个实例
 	row.Scan(&user.id, &user.name)
 	fmt.Println(user.id, user.name)
 
+	res_list := getRes(db, "select * from test")
+	fmt.Println("结果集是", res_list)
+
+}
+
+//返回map结构集
+// 驱动默认不是返回map类的结果，这就需要我们出利用返回的结果信息来构造结果为map，结果集的Columns函数返回返回了哪些列
+func getRes(db *sql.DB, sql string) (res []map[string]interface{}) {
+	rst, err := db.Query(sql)
+	checkError(err)
+	cols, _ := rst.Columns()
+	cols_len := len(cols) //字段的个数
+	//每一行的结果
+	values := make([]string, cols_len)
+	ptr := make([]interface{}, cols_len)
+	for rst.Next() {
+		for i := 0; i < cols_len; i++ {
+			ptr[i] = &values[i] //存放每一行的结果内存地址
+		}
+		rst.Scan(ptr...) //将每个字段的值映射到ptr中
+		row := make(map[string]interface{})
+		//将每行的值和字段进行映射
+		for k, col := range cols {
+			// val := values[k]
+			// fmt.Println(val)
+			row[col] = values[k]
+		}
+		res = append(res, row)
+	}
+	return
+}
+
+func show_process() {
+	db.SetMaxIdleConns(2)
+	for i := 0; i < 10; i++ {
+		go func() {
+			db.Ping()
+		}()
+	}
+	time.Sleep(20 * time.Second)
 }
