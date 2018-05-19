0df61c938d5515f220587cb302f3d2942b4e913a
daheige <zhuwei313@hotmail.com>
2018-05-19 23:14:43 +0800

    更新grpc客户端调用

--- protobuf/inf_client.go
+++ protobuf/inf_client.go
@@ -17,7 +17,7 @@ func main() {
 	//create a connection
 	conn, err := grpc.Dial(address, grpc.WithInsecure())
 	if err != nil {
-		log.Fatal("did not connect: %v", err)
+		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
 	c := pb.NewGreeterClient(conn) //create Greet service Client
@@ -28,7 +28,7 @@ func main() {
 	}
 	r, err := c.SayHello(context.Background(), &pb.HelloRequest{Name: name})
 	if err != nil {
-		log.Fatal("could not greet: %v", err)
+		log.Fatalf("could not greet: %v", err)
 	}
 	log.Printf("Greeting: %s", r.Message)
 }
