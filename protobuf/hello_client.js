8122bc5c289f75aafa907b0cf390a390863c52fe
daheige <zhuwei313@hotmail.com>
2018-06-13 23:38:41 +0800

    新增nodejs调用方式

--- protobuf/hello_client.js
+++ protobuf/hello_client.js
@@ -24,12 +24,30 @@ const client = new proto.Greeter(host + ":" + port, grpc.credentials.createInsec
 // console.log(typeof proto.HelloRequest);
 
 //执行rpc调用
-var request = new proto.HelloRequest();
-request.setName("daheige")
-request.setUa(2)
-client.SayHello(request, function(err, data) {
+// var request = new proto.HelloRequest();
+// request.setName("daheige")
+// request.setUa(2)
+
+//method1:
+/*obj as params
+client.SayHello({
+    name: "daheige",
+    ua: 2,
+}, function(err, data) {
     if (err) console.log(err)
     console.log("reply data : ", data)
     console.log(data.message)
     grpc.closeClient(client);
+});
+*/
+
+//method2:
+client.SayHello({
+    name: "daheige",
+    ua: 2,
+}, function(error, data) {
+    if (error) {
+        console.log(error)
+    }
+    console.log(data)
 });
